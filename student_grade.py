a2b6ef85678997cc7c425d709037f58995e6cb11
Ashish Bhosale <bhosaleashish835@gmail.com>
2026-09-24 23:54:21 +0530

    Create student_grade.py

---
+++ student_grade.py
@@ -0,0 +1,14 @@
+marks = float(input("Enter your marks: "))
+
+if marks >= 90:
+    grade = "A+"
+elif marks >= 80:
+    grade = "A"
+elif marks >= 70:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+else:
+    grade = "F"
+
+print("Grade:", grade)
